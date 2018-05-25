bb1520dc9c6a41ad1bcacbd4a8da4bbcd4d16f58
Rasmus Rendal <rasmus@rend.al>
2018-05-25 12:42:54 +0200

    Improved sorting

--- Frequency.py
+++ Frequency.py
@@ -39,82 +39,114 @@ def sort_dict(to_sort):
 
 
 def count_occurences(text):
-    frequency_list = {}
+    frequency_list = {
+            'A': 0,
+            'B': 0,
+            'C': 0,
+            'D': 0,
+            'E': 0,
+            'F': 0,
+            'G': 0,
+            'H': 0,
+            'I': 0,
+            'J': 0,
+            'K': 0,
+            'L': 0,
+            'M': 0,
+            'N': 0,
+            'O': 0,
+            'P': 0,
+            'Q': 0,
+            'R': 0,
+            'S': 0,
+            'T': 0,
+            'U': 0,
+            'V': 0,
+            'W': 0,
+            'X': 0,
+            'Y': 0,
+            'Z': 0,
+    }
+    regex = re.compile('[^a-zA-Z ]')
+    text = regex.sub('', text)
+    text = text.upper()
+
     total_occurences = 0
     for c in text:
         if c in frequency_list:
             frequency_list[c] += 1
-        elif c is not ' ':
-            frequency_list[c] = 1
         if c is not ' ':
             total_occurences += 1
     return (frequency_list, total_occurences)
 
 
 def print_table(frequencies, sort):
-    print("| Char | Frequency | English Frequency |")
-    print("----------------------------------------")
-    keys = []
-    if sort:
+    print("| Char | Frequency | English Frequency | English Char |")
+    print("-------------------------------------------------------")
+    if not args.sort:
+        keys = list(frequencies.keys())
+        english_keys = list(english_frequency.keys())
+    else:
         keys = sort_dict(frequencies)
         english_keys = sort_dict(english_frequency)
-    else:
-        keys = frequencies.keys()
-        english_keys = english_frequency.keys()
-    for c in keys:
-        n = ord(c)
+    m = len(keys)
+    print(keys)
+    print(len(keys))
+    if m > 26:
+        m = 26
+    for n in range(m):
+        c = keys[n]
+        english_c = english_keys[n]
         freq = 0
         if c in per_list:
             freq += per_list[c]*100
-        if chr(n+32) in per_list:
+        if chr(ord(c)+32) in per_list:
             freq += per_list[chr(n+32)]*100
         freq_str = str(freq)[:5]
         freq_str += "0"*(6-len(freq_str))
-        efreq_str = str(english_frequency[c])[:5]
+        efreq_str = str(english_frequency[english_c])[:5]
         efreq_str += "0"*(6-len(efreq_str))
-        print('| ' + c + '    |   ' + freq_str + '% |           ' + efreq_str + '% |')
+        print('| ' + c + '    |   ' + freq_str + '% |           ' + efreq_str + '% |            ' + english_c + ' |')
 
 
-parser = argparse.ArgumentParser(description='Count the frequency of characters in a text file')
-parser.add_argument('text', metavar='Text', nargs='*',
-                    help='The text to parse')
-parser.add_argument('--file', dest='file', nargs=1,
-                    help='Read text from file')
-parser.add_argument('--sort', dest='sort', action='store_true',
-                    help='Sort the charaters when printing')
-parser.add_argument('--ic', dest='ic', action='store_true',
-                    help='Calculate the index of coincidence')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Count the frequency of characters in a text file')
+    parser.add_argument('text', metavar='Text', nargs='*',
+                        help='The text to parse')
+    parser.add_argument('--file', dest='file', nargs=1,
+                        help='Read text from file')
+    parser.add_argument('--sort', dest='sort', action='store_true',
+                        help="Sort the letters by frequency, instead of alphabetically")
+    parser.add_argument('--ic', dest='ic', action='store_true',
+                        help='Calculate the index of coincidence')
 
-args = parser.parse_args()
-text = ""
-if args.text:
-    text = ' '.join(args.text)
-elif args.file:
-    with open(args.file[0], 'r') as myfile:
-        text=myfile.read().replace('\n', '')
-else:
-    raise Exception('No text given')
-
-regex = re.compile('[^a-zA-Z ]')
-text = regex.sub('', text)
-text = text.upper()
-
-frequency_list, total_occurences = count_occurences(text)
+    args = parser.parse_args()
+    text = ""
+    if args.text:
+        text = ' '.join(args.text)
+    elif args.file:
+        with open(args.file[0], 'r') as myfile:
+            text=myfile.read().replace('\n', '')
+    else:
+        raise Exception('No text given')
 
 
-per_list = {}
-for c in frequency_list:
-    per_list[c] = frequency_list[c]/total_occurences
+    frequency_list, total_occurences = count_occurences(text)
 
-if args.ic:
-    phi_o = 0
-    for n in range(ord('A'), ord('Z')):
-        c = chr(n)
-        if c in frequency_list:
-            phi_o += (frequency_list[c]/total_occurences)*((frequency_list[c]-1)/(total_occurences-1))
-    print("Index of Coincidence: " + str(phi_o))
-    print("Common IC's:")
-    print("English plaintext ≈ 0.066")
-    print("Vigenere Cipher ≈ 0.042")
-else:
-    print_table(frequency_list, args.sort)
+
+    per_list = {}
+    for c in frequency_list:
+        per_list[c] = frequency_list[c]/total_occurences
+
+    if args.ic:
+        phi_o = 0
+        for n in range(ord('A'), ord('Z')):
+            c = chr(n)
+            if c in frequency_list:
+                phi_o += (frequency_list[c]/total_occurences)*((frequency_list[c]-1)/(total_occurences-1))
+        print("Index of Coincidence: " + str(phi_o))
+        print("Common IC's:")
+        print("English plaintext ≈ 0.066")
+        print("Vigenere Cipher ≈ 0.042")
+    else:
+        print_table(frequency_list, args.sort)
