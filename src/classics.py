baa4ea128c2d0d4be16205c7f068678fc91ba4a7
Rasmus Rendal <rasmus@rend.al>
2019-10-09 08:50:35 +0200

    Added ROT13

---
+++ src/classics.py
@@ -0,0 +1,25 @@
+def rot(ciphertext, n=13):
+    out = ""
+    for c in ciphertext:
+        v = ord(c)
+        upper = True
+        if v >= ord('a'):
+            v -= ord('a') - ord('A')
+            upper = False
+
+        if not (v >= ord('A') and v <= ord('Z')):
+            out += c
+            continue
+
+        v -= ord('A')
+        v += n
+        v %= ord('Z') - ord('A')+1
+        if upper:
+            v += ord('A')
+        else:
+            v += ord('a')
+        out += chr(v)
+    return out
+
+def rot13(ciphertext):
+    return rot(ciphertext, 13)
