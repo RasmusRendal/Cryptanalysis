bae1b6d4119ef91132cc7c87b6f66f9cad589340
Rasmus Rendal <rasmus@rend.al>
2021-03-25 09:24:50 +0100

    Document classics package

--- cryptanalysis/classics.py
+++ cryptanalysis/classics.py
@@ -1,4 +1,13 @@
-def rot(ciphertext, n=13):
+#!/usr/bin/env python3
+"""Module for miscellaneous classical ciphers"""
+
+def rot(ciphertext: str, n: int = 13) -> str:
+    """Uses the classical rot algorithm to encode a string
+
+    Args:
+        ciphertext: The string to decode
+        n: How far to rotate
+    """
     out = ""
     for c in ciphertext:
         v = ord(c)
@@ -22,5 +31,9 @@ def rot(ciphertext, n=13):
     return out
 
 
-def rot13(ciphertext):
+def rot13(ciphertext: str) -> str:
+    """The rot13 encoding.
+
+    Shorthand for ``rot(ciphertext, 13)``
+    """
     return rot(ciphertext, 13)
---
+++ docs/modules/classics.rst
@@ -0,0 +1,5 @@
+Classics
+========
+
+.. automodule:: cryptanalysis.classics
+    :members:
